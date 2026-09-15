7a24102fd27eaca47e5bdaf4e2796809dd32daaa
ams-sth <sthaamsh37@gmail.com>
2026-09-15 17:09:44 +1000

    Add ChangePasswordRequest DTO for upcoming change-password endpoint

--- splitsync/server/Dtos/AuthDtos.cs
+++ splitsync/server/Dtos/AuthDtos.cs
@@ -4,4 +4,6 @@ public record RegisterRequest(string Email, string Password);
 
 public record LoginRequest(string Identifier, string Password);
 
+public record ChangePasswordRequest(string CurrentPassword, string NewPassword);
+
 public record AuthResponse(string Token, string Username, string Email);
