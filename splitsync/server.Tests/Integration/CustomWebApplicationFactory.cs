3f64528413f126cde16931476dac162d69ec280b
shantimoktan <shantimoktan663@gmail.com>
2026-09-15 15:47:08 +1000

    add CustomWebApplicationFactory for integration tests and add In Memory database package for testing without postgres database

---
+++ splitsync/server.Tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SplitSync.Api.Data;
+
+namespace SplitSync.Api.Tests.Integration;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly string _dbName = Guid.NewGuid().ToString();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<DbContextOptions<AppDbContext>>();
+            services.RemoveAll<IDbContextOptionsConfiguration<AppDbContext>>();
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase(_dbName));
+        });
+    }
+}
